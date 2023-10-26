f239f34132f0e15d6df22d3901bc7b43e22277b0
Sumanth Kumar Sivadi <sivadisumanth@gmail.com>
2023-10-25 21:45:38 -0500

    added computation.js

---
+++ routes/computation.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+    router.put( m = Math.round(Math.random()*10));
+    router.put( n = Math.round(Math.random()*10));
+    router.post(a=Math.hypot(m,n));
+    router.post(b=Math.ceil(m));
+    router.post(c=Math.clz32(n));
+    res.send(`hypot applied to ${m} and ${n} is ${a}`);
+    res.send(`ceil applied to ${m} is ${b}`);
+    res.send(`clz32 applied to ${n} is ${c}`);
+
+});
+
+module.exports = router;
+let m;
+let n;
+let a;
+let b;
+let c;
+
